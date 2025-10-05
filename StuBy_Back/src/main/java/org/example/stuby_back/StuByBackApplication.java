44354cf7624922595bb5bca85857d38480bed91e
DESKTOP-ULI9UE4\USER <cleo708028@gmail.com>
2025-10-05 19:34:38 +0900

    first commit

---
+++ StuBy_Back/src/main/java/org/example/stuby_back/StuByBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.stuby_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuByBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuByBackApplication.class, args);
+    }
+
+}
---
+++ StuBy_Back/src/test/java/org/example/stuby_back/StuByBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stuby_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuByBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
