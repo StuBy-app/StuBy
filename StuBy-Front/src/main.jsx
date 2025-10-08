e9bedf5f21a6f2910c9d7eaa5caf2752948ab177
Mi-Kyung331 <uu.3120@gmail.com>
2025-10-08 11:17:30 +0900

    npm install 및 main 작성

--- StuBy-Front/src/main.jsx
+++ StuBy-Front/src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { Global } from "@emotion/react";
+import { global } from "./styles/global.js";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60,
+      retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <>
+          <Global styles={global} />
+          <App />
+        </>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </>
 )
